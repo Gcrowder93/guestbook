957f64635f96f3e1684064019d9ef74bf88169a5
Chase Crowder <gcrowder93@gmail.com>
2022-02-09 12:28:01 -0800

    dark mode working properly

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Home from './Views/Home';
+import DarkModeToggle from './Components/DarkMode/DarkModeToggle';
 
 function App() {
   return (
     <div className="App">
+      <div className="navbar">
+        <DarkModeToggle />
+      </div>
       <Home />
     </div>
   );
