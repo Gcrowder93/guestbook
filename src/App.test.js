5783ed6a855a96521daec219d59bd19c066ebf00
Chase Crowder <gcrowder93@gmail.com>
2022-02-08 17:02:46 -0800

    app test pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { EntryProvider } from './Context/EntryContext';
+import { UserProvider } from './Context/UserContext';
 
-test.skip('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders learn react link', () => {
+  render(
+    <UserProvider>
+      <EntryProvider>
+        <App />
+      </EntryProvider>
+    </UserProvider>
+  );
+  const linkElement = screen.getByText(/add an entry/i);
   expect(linkElement).toBeInTheDocument();
 });
